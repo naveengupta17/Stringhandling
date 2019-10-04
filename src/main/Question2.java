dd4976c53401783707ba69219a18051093783e70
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2019-10-04 22:43:01 +0530

    printing output

--- src/main/Question2.java
+++ src/main/Question2.java
@@ -1,4 +1,14 @@
 package main;
 
+import java.util.Scanner;
+
 public class Question2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your String and last index of Substring ");
+        String s = scan.nextLine().trim();
+        int n = scan.nextInt();
+        System.out.println(s.substring(0, n));
+        scan.close();
+    }
 }
