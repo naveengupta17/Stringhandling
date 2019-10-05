4737034a4fd7c82e98f31efb3672c7a45ece0856
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2019-10-05 19:49:54 +0530

    printing output

--- src/main/Question5.java
+++ src/main/Question5.java
@@ -1,4 +1,17 @@
 package main;
 
+import java.util.Scanner;
+
 public class Question5 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the string");
+        String s = scan.nextLine().trim();
+        System.out.println("Enter the substring to replace :");
+        String s1 = scan.nextLine().trim();
+        String s2 = s.replaceAll(s1, "#replaced");
+        System.out.println(s2);
+
+
+    }
 }
