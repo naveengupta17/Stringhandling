eb0577b955b593031e47cb5f063ce19f5419dd5e
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2019-10-05 20:23:32 +0530

    Calculating totalWeight

--- src/main/Question7.java
+++ src/main/Question7.java
@@ -1,4 +1,21 @@
 package main;
 
+import java.util.Scanner;
+
 public class Question7 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the String ");
+        String s = scan.nextLine().trim();
+        char[] arr = s.toCharArray();
+        int totalWeight = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int a = arr[i];
+            totalWeight = totalWeight + a;
+
+
+        }
+        System.out.println("totalWeight of String is :" + totalWeight);
+
+    }
 }
