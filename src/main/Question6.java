915e564effe298ec5b3ed76e0474f385bc6a3e9c
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2019-10-05 20:01:48 +0530

    printing output

--- src/main/Question6.java
+++ src/main/Question6.java
@@ -1,4 +1,12 @@
 package main;
 
+import java.util.Scanner;
+
 public class Question6 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your String");
+        String s2 = scan.nextLine().trim();
+        System.out.println(s2.toUpperCase());
+    }
 }
