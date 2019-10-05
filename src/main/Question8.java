85e063243612329cc6c2de903ed183819c9bed7e
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2019-10-05 20:34:04 +0530

    printing upperWeight

--- src/main/Question8.java
+++ src/main/Question8.java
@@ -1,6 +1,27 @@
 package main;
 
+import java.util.Scanner;
+
 public class Question8 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the String ");
+        String s = scan.nextLine().trim();
+        s = s.toUpperCase();
+        char[] arr = s.toCharArray();
+        int upperWeight = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int a = arr[i];
+            upperWeight = upperWeight + a;
 
 
+        }
+        System.out.println("upperWeight of String is :" + upperWeight);
+
+    }
 }
+
+
+
