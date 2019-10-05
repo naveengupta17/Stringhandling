2ea55dbb42d83ca059f300cdaee7bf626429fe67
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2019-10-05 21:23:08 +0530

    printing index

--- src/main/Question10.java
+++ src/main/Question10.java
@@ -1,5 +1,16 @@
 package main;
 
+
+import java.util.Scanner;
+
 public class Question10 {
+    public static void main(String[] args) {
+        System.out.println("enter the string :");
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine().trim();
+        System.out.println("enter the character :");
+        char s1 = scan.nextLine().charAt(0);
+        System.out.println(s.indexOf(s1));
+    }
 
 }
